5e204bc96881673b91e4828ff5b80577f5b6636b
AshCloud2123 <150422810+AshCloud2123@users.noreply.github.com>
2025-06-05 00:07:29 +0800

    c

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,12 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
-      {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+      <Stack.Screen name="index" options={{ title: 'Login', headerShown: false }} />
+      <Stack.Screen name="SignUp" options={{ title: 'Sign Up' }} />
+      <Stack.Screen name="Home" options={{ title: 'My Notes', headerLeft: () => null }} />
+      <Stack.Screen name="NoteDetail" options={{ title: 'Note Detail' }} />
+      <Stack.Screen name="NoteEdit" options={{ title: 'Edit Note' }} />
+      <Stack.Screen name="+not-found" options={{ title: 'Not Found' }} />
     </Stack>
   );
 }
